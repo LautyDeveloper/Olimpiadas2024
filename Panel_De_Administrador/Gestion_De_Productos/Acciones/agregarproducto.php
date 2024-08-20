c48a8b8af29c4e1a6e0d84c8e7fdb5eef99ba4ab
unknown <=>
2024-08-19 21:24:20 -0300

    Sigo

--- Panel_De_Administrador/Gestion_De_Productos/Acciones/agregarproducto.php
+++ Panel_De_Administrador/Gestion_De_Productos/Acciones/agregarproducto.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregando Producto</title>
+    <link rel="stylesheet" href="../Estilos/agregar-producto.css">
 </head>
 <body>
     <h1>Agregando Producto...</h1>
